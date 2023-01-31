be12a2602f40283996a66e61ffefae7f6d4d0d6a
Eduardo Villaverde <eduardo.villaverde@dcsl.com>
2023-01-31 14:36:44 +0100

    Adding .env file and giphy api key in it+

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./Components/gif/GifExpertApp";
 import "./styles.css";
 
+console.log(import.meta.env);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <GifExpertApp />
