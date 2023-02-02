04e6c4462d402cb248989cd12b1ffa1674304173
eduvilla97 <eduvilla99@gmail.com>
2023-02-02 16:59:53 +0100

    clean comments

--- src/Components/gif/GifItem.tsx
+++ src/Components/gif/GifItem.tsx
@@ -5,9 +5,6 @@ interface GifItemProps {
 
 export const GifItem = ({ title, url }: GifItemProps) => {
   return (
-    // <li>{title}</li>
-    // <img src={gif.url} key={uuid()} />
-
     <div className="card">
       <img src={url} alt={title} />
       <p>{title}</p>
